d44ea8941979332e7985bc67d6e0a70271dcbae2
Chris Campbell <chris@ccampbell.co>
2017-03-04 13:11:52 +0000

    Searchbar setup, refactor to ES6 syntax and initial filestructure for new components.@

---
+++ src/components/vid_detail.js
@@ -0,0 +1,3 @@
+/**
+ * Created by chris on 04/03/2017.
+ */
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/searchbar';
+
+const API_KEY = 'AIzaSyDtVa7xP1YFhge1ApGoR0JWVENKd3wvLCw';
+
 
 //Create a new component.  Should produce some HTML.
 
-const App = function() {
-    return <div>Hi!</div>;
+const App = () => {
+    return <div>
+        <SearchBar />
+    </div>;
 }
 
 //Take the component's generated H TML and put it on the page.
 
-ReactDOM.render(App);
+
+ReactDOM.render(<App />, document.querySelector('.container')) ;
 
 
