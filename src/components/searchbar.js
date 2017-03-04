8e5882b37db83a2af147a23e4f749659e4d2cab2
Chris Campbell <chris@ccampbell.co>
2017-03-04 19:33:19 +0000

    End of Section 1

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,11 +4,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: 'Some stuff'};
+    }
 
     render() {
-        return  <input />;
+        return (
+    <div>
+        <input
+            value={this.state.term}
+            onChange={ event => this.setState({term: event.target.value})} />
+
+        </div>
+        );
     }
 }
+
+
+
 
 
 export default SearchBar;
