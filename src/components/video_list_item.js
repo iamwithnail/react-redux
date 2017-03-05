7d65ddeace1254524f7bffdccfa0779772b80816
Chris Campbell <chris@ccampbell.co>
2017-03-05 20:38:18 +0000

    Working li select and video player, to lecture 29.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,3 +1,28 @@
 /**
  * Created by chris on 04/03/2017.
  */
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) => {
+    // that {video} means that the first props has a property called video
+    // equivalent to const video = props.video;
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return  (
+        <li onClick={ () => onVideoSelect(video)} className="list-group-item">
+                <div className="video-list media">
+                    <div className="media-left">
+                        <img className="media-object" src={imageUrl}/>
+
+                    </div>
+                    <div className="media-body">
+                    <div className="media-heading">
+                        {video.snippet.title}
+                    </div>
+                </div>
+    </div>
+            </li>
+    );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component}from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchbar';
-
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyDtVa7xP1YFhge1ApGoR0JWVENKd3wvLCw';
+import VideoList from './components/video_list'
+import VideoDetail from './components/vid_detail'
 
 
-//Create a new component.  Should produce some HTML.
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>;
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        YTSearch({key: API_KEY, term: 'failarmy'},(videos) =>  {
+        this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+        });
+    //in ES6 this single 'videos' resolves to this.setState({videos:videos});
+    });
+
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos}/>
+
+            </div>
+        );
+    }
 }
 
 //Take the component's generated H TML and put it on the page.
